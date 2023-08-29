3c88e78394ff70889ce6f7eaf18368f169756204
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-08-30 00:49:18 +0330

    step4

--- script.js
+++ script.js
@@ -1,67 +1,54 @@
 const addbtn=document.querySelector(".addbutton");
 const todolist=document.querySelector(".list");
 const mbody=document.getElementById('mainbody');
-const num1=document.querySelector(".number");
+//const num1=document.querySelector(".number");
 const inputvalue=document.querySelector(".todoinput");
-
 var i=0;
 function addnewbtn(){
     
     const newdiv=document.createElement("div");
     if(inputvalue.value){
-        
         newdiv.classList.add("list");
         mbody.appendChild(newdiv);
-        const num2=document.createElement("div")
-        num2.classList.add("number");
-        num2.innerHTML=num1.textContent;
-        num1.innerHTML=Number(num1.textContent)+1;
-        newdiv.appendChild(num2);
-    
         const input =document.createElement("input");
         input.classList.add("todoinput");
         input.setAttribute('type','test');
         input.setAttribute('value',inputvalue.value)
         input.setAttribute('id',i);
+        input.setAttribute('disabled','');
         newdiv.appendChild(input);
+        newdiv.setAttribute('status','active');
         inputvalue.value="";
         i++;
         const deletebtn=document.createElement('button');
         deletebtn.classList.add("deletebtn");
         deletebtn.innerHTML='Delete';
-        // newdiv.append(deletebtn);
-        deletebtn.addEventListener('click', deletefunc)
-        function deletefunc(){
+        deletebtn.addEventListener('click', function(){
             newdiv.remove();
-            const countdiv=document.getElementsByClassName("list").length;
-            num1.innerHTML=countdiv;
-            for(let i = 1; i < countdiv; i++){
-                const conti=document.getElementsByClassName("number")[i];
-                conti.innerHTML=i;
-                }
-            i--;
-        }
-        const Addbtn=document.createElement('button');
-        Addbtn.classList.add("addbtn");
-        Addbtn.innerHTML='Add';
-        Addbtn.addEventListener('click',function(){
-            input.setAttribute('disabled','');
         });
+        const Editbtn=document.createElement('button');
+        Editbtn.classList.add("addbtn");
+        Editbtn.innerHTML='Edit';
+        Editbtn.addEventListener('click',function(){
+            if(input.hasAttribute('disabled')){
+                input.removeAttribute('disabled','');
+            }
+            else{
+                input.setAttribute('disabled','');
+            }
+        });
+
+
         const Donebtn=document.createElement('button');
         Donebtn.classList.add("donebtn");
         Donebtn.innerHTML='Done';
         Donebtn.addEventListener('click',function(){
             input.style.textDecoration = "line-through";
             input.setAttribute('disabled','');
+            newdiv.setAttribute('status','completed');
         });
-        const Editbtn=document.createElement('button');
-        Editbtn.classList.add("addbtn");
-        Editbtn.innerHTML='Edit';
-        Editbtn.addEventListener('click',function(){
-            input.style.textDecoration = "none";
-            input.removeAttribute('disabled','');
-        })
-        newdiv.append(Addbtn,deletebtn,Donebtn,Editbtn);
+        
+        newdiv.append(Editbtn,deletebtn,Donebtn);
     
     console.log(i);
     }else{
@@ -70,8 +57,48 @@ function addnewbtn(){
     
 }
 
-
 addbtn.addEventListener('click',addnewbtn);
 
-console.log(i);
+const allnav=document.querySelector('.allnav');
+const activenav=document.querySelector('.activenav');
+const completednav=document.querySelector('.completednav');
 
+allnav.addEventListener("click", function (e) {
+    allnav.classList.add('activeNavClick');
+    activenav.classList.remove('activeNavClick');
+    completednav.classList.remove('activeNavClick');
+    const listbox = Array.from(document.getElementsByClassName('list'));
+    listbox.forEach(list => {
+        list.style.display='flex';
+    });
+
+});
+activenav.addEventListener("click", function (e) {
+    allnav.classList.remove('activeNavClick');
+    activenav.classList.add('activeNavClick');
+    completednav.classList.remove('activeNavClick');
+    const alllist=document.querySelectorAll(".list");
+    alllist.forEach(todolist => {
+        todolist.style.display='none';
+        if (todolist.getAttribute('status')== 'active'){
+            console.log(todolist.getAttribute('status'));
+            todolist.style.display='flex';
+        }
+    });
+
+});
+
+completednav.addEventListener("click", ()=> {
+    allnav.classList.remove('activeNavClick');
+    activenav.classList.remove('activeNavClick');
+    completednav.classList.add('activeNavClick');
+    const alllist=document.querySelectorAll(".list");
+    alllist.forEach(todolist => {
+        todolist.style.display='none';
+        if (todolist.getAttribute('status')== 'completed'){
+            console.log(todolist.getAttribute('status'));
+            todolist.style.display='flex';
+        }
+    });
+
+});
